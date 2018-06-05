79d62b3eaa26734f8f011a5fbcf257b2f56709a8
shlomo zucker <39903252+ShlomoZucker@users.noreply.github.com>
2018-06-05 11:32:59 +0300

    Update Processor.java

--- Processor.java
+++ Processor.java
@@ -6,8 +6,8 @@ import processing.Orders.Order;
 import java.io.File;
 
 /**
- * this is a wrapper class for Filter and Order and allows us to have two of them together and thus avoid
- * problems.
+ * this is a wrapper class for Filter and Order and allows us to have the both of them together under
+ * one parent classe, which allows us to work on them and process them using the same bunch of code.
  */
 public class Processor {
     private Filter filter;
