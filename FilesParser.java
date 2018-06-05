57c345eacc6cb688c6fe85192a551143250a635b
shlomo zucker <39903252+ShlomoZucker@users.noreply.github.com>
2018-06-05 11:28:04 +0300

    Update FilesParser.java

--- FilesParser.java
+++ FilesParser.java
@@ -2,6 +2,9 @@ package processing;
 
 import java.io.File;
 
+/**
+* returns all the files, from the folder of a given path.
+/*
 public class FilesParser {
 
     public static File[] ParseFiles(String sourcedir){
