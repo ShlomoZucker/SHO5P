89fc8e3f67a7d3931d15a1e14539632bd3ee7287
shlomo zucker <39903252+ShlomoZucker@users.noreply.github.com>
2018-06-05 11:30:14 +0300

    Update OverLord.java

--- OverLord.java
+++ OverLord.java
@@ -6,12 +6,15 @@ import processing.Orders.OrderFactory;
 
 import java.io.File;
 
+/**
+* runs everything! parses the files, filters them, and orders them, all according to CMDfile.
+*/
 public class OverLord {
-    private final static int FILE_DIRECONRY = 0;
+    private final static int FILE_DIRECTORY = 0;
     private final static int COMAND_FILE = 0;
 
     public static void main(String[] args) {
-        File[] files = FilesParser.ParseFiles(args[FILE_DIRECONRY]);
+        File[] files = FilesParser.ParseFiles(args[FILE_DIRECTORY]);
         Order order = OrderFactory.createOrder("size");
         Filter filter = FilterFactory.createFilter("between#23#-80");
         File[] filesToPrint = order.runOrder(filter.runFilter(files));
