c943db70e8d0896fb3170d9027ae1226f6b5ec2d
shlomo zucker <39903252+ShlomoZucker@users.noreply.github.com>
2018-06-05 11:35:49 +0300

    Update bad.java

--- bad.java
+++ bad.java
@@ -1,12 +1,26 @@
 package processing;
 
+/**
+* an error for bad formatting, e.g. FiLtEr instead of FILTER.
+*/
 class BAD_FORMAT_ERROR extends RuntimeException{
-    private String missingPart;
-    private int line;
+    // paramaters -
+    
+    private String missingPart;             // what's missing
+    
+    private int line;                       // where is it missing
+    
+    /**
+    * default constructor.
+    */
     BAD_FORMAT_ERROR(String part, int atLine){
         missingPart = part;
         line = atLine;
     }
+    
+    /** 
+    * prints out the error.
+    */
     public void printError(){
         System.out.print("Bad " + missingPart + " at line: " + line);
     }
